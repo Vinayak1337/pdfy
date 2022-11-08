be7df825223d064b5d4d80b370909f4827fc5a90
Vinayak <34209962+Vinayak1337@users.noreply.github.com>
2022-11-08 18:37:19 +0530

    btn's width full important

--- app/page.tsx
+++ app/page.tsx
@@ -68,9 +68,9 @@ export default function Home() {
 					<Input />
 					<Input />
 					<div className='mt-10 flex gap-5 w-full justify-evenly flex-wrap'>
-						<ThemeBtn className='w-full md:w-fit'>Automate PDF tasks</ThemeBtn>
-						<ThemeBtn className='w-full md:w-fit'>Download PDF</ThemeBtn>
-						<ThemeBtn className='w-full md:w-fit'>Download .CSV</ThemeBtn>
+						<ThemeBtn className='!w-full md:w-fit'>Automate PDF tasks</ThemeBtn>
+						<ThemeBtn className='!w-full md:w-fit'>Download PDF</ThemeBtn>
+						<ThemeBtn className='!collapsew-full md:w-fit'>Download .CSV</ThemeBtn>
 					</div>
 				</div>
 			</div>
